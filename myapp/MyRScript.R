a9bfa5d0d7787bf6d248e318f920726ef9f6dfc1
Albert <akutej@gmail.com>
2022-12-20 00:09:22 +0100

    dataTable

---
+++ myapp/MyRScript.R
@@ -0,0 +1,5 @@
+#read.table("example.csv", header=TRUE, sep=";", dec=".")
+#testtable <- read.csv(file = 'myapp/Data/data.csv')
+
+testtable <- read.table("myapp/Data/data.csv", header=TRUE, sep=";", dec=".")
+head(testtable)
