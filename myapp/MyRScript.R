4735c120fec244d56a28812f96ae36898992807c
Albert <akutej@gmail.com>
2022-12-25 23:03:54 +0100

    done

--- myapp/MyRScript.R
+++ myapp/MyRScript.R
@@ -1,7 +1,12 @@
 #read.table("example.csv", header=TRUE, sep=";", dec=".")
-#testtable <- read.csv(file = 'myapp/Data/data.csv')
-
-testtable <- read.table("Data/data.csv", header=TRUE, sep=";", dec=".")
+testtable <- read.csv(file = 'myapp/Data/data.csv', header=TRUE, sep=";", dec=".")
+accounttable <- read.csv(file = 'myapp/Data/account.csv', header=TRUE, sep=";", dec=".")
+#testtable <- read.table("Data/data.csv", header=TRUE, sep=";", dec=".")
 #accounttable <- read.table("Data/account.csv", header=TRUE, sep=";", dec=".")
-head (testtable)
+#head (testtable)
 #head (accounttable)
+accframe=as.data.frame.matrix(accounttable)
+newtest <- aggregate(accframe, by=list(accounttable$ACC2SURV_RATEGUI), FUN=length )
+#reduced <- subset(newtest, select=c("Acc_ID"))
+newtest(ACC_ID)
+  
