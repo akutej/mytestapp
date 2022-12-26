6093b100b0caca65894a945d704549bff21178c9
Albert <akutej@gmail.com>
2022-12-27 00:39:32 +0100

    Auswertung Alter

--- myapp/MyRScript_list filteredAGE.R
+++ myapp/MyRScript_list filteredAGE.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(ggplot2)
+library(scales)
 #read.table("example.csv", header=TRUE, sep=";", dec=".")
 testtable <- read.csv(file = 'myapp/Data/data.csv', header=TRUE, sep=";", dec=".")
 accounttable <- read.csv(file = 'myapp/Data/account.csv', header=TRUE, sep=";", dec=".")
@@ -8,7 +11,9 @@ accounttable <- read.csv(file = 'myapp/Data/account.csv', header=TRUE, sep=";", 
 accframe=as.data.frame.matrix(accounttable)
 newtest <- aggregate(accframe, by=list(accounttable$ACC2SURV_RATEGUI), FUN=length )
 #reduced <- subset(newtest, select=c("Acc_ID"))
-colnames(newtest)
+
+#colnames(newtest)
+
 #names(newtest)[2] <- "User"
 #newtest[2]
 #accounttable[1:5]
@@ -16,7 +21,8 @@ colnames(newtest)
 
 
 accframe2 <- cbind(accframe,AGEGroup=NA)
-accframe3 <- accframe2 %>% filter(!is.na(ACC2SURV_INFOAGE))
+accframe3 <- accframe2 %>% filter(!is.na(ACC2SURV_RATEGUI))
+accframe3 <- accframe3 %>% filter(!is.na(ACC2SURV_INFOAGE))
 accframe4 <- accframe3 %>% 
   mutate (AGEGroup = case_when(
     #ACC2SURV_INFOAGE == "53" ~ 'ALT',
@@ -27,5 +33,22 @@ accframe4 <- accframe3 %>%
     between(ACC2SURV_INFOAGE,61,70) ~ "AGE 61-70"
   ))
 
+accframe4 <- accframe4 %>% 
+  mutate (ACC2SURV_RATEGUI = case_when(
+    ACC2SURV_RATEGUI == "1" ~ 'classic',
+    ACC2SURV_RATEGUI == "2" ~ 'graphic',
+    ACC2SURV_RATEGUI == "3" ~ 'equal'
+  ))
+
 #accframe3 %>% select(ACC2SURV_INFOAGE)
-accframe4 %>% select(ACC2SURV_INFOAGE, AGEGroup)
+accframe4 <- accframe4 %>% select(ACC2SURV_RATEGUI,ACC2SURV_INFOAGE, AGEGroup)
+#accframe4
+newtest2 <- aggregate(accframe4, by=list(accframe4$AGEGroup,accframe4$ACC2SURV_RATEGUI), FUN=length)
+newtest2
+
+#print(nrow(accframe4))
+#rows <- as.numeric(count(accframe4))
+#print(rows)
+ggp <- ggplot(newtest2, aes(x = reorder(Group.2, -ACC2SURV_INFOAGE), y = ACC2SURV_INFOAGE,  fill = Group.1, label = ACC2SURV_INFOAGE)) +  # Create stacked bar chart
+  geom_bar(stat = "identity")
+ggp + geom_text(size = 3, position = position_stack(vjust = 0.8)) + labs(title = "Bewertungen") + labs(x = "Methoden")+ labs(y = "Personen")+scale_fill_discrete(name = "Altersgruppen")
