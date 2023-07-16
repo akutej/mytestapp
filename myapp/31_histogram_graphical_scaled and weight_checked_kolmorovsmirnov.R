b4819822d22577b7f48583b94c2db0a96072053b
Albert <akutej@gmail.com>
2023-07-16 22:17:12 +0200

    ok

--- myapp/31_histogram_graphical_scaled and weight_checked_kolmorovsmirnov.R
+++ myapp/31_histogram_graphical_scaled and weight_checked_kolmorovsmirnov.R
@@ -3,7 +3,7 @@ library(openxlsx)
 library(gplots)
 library(ggplot2)
 
-answerstable <- read.csv(file = 'myapp/Data/RQ1_corrected.csv', header=TRUE) #importiere das answers file
+answerstable <- read.csv(file = 'myapp/data/RQ1_corrected_scaled.csv', header=TRUE) #importiere das answers file
 dfall <- answerstable %>% filter(QUES2SURV_METHOD == "classic" & ANS2SURV_ANSWERED == 1)
 scenarios <- as.data.frame(table(dfall$QUES_ID))
 numberscenarios  <- nrow(scenarios)
