d597d8bafe0d02a7dfe32c49d3adf4fd24747f93
Albert <akutej@gmail.com>
2023-07-30 20:55:39 +0200

    ok

---
+++ myapp/tex/300_together.R
@@ -0,0 +1,26 @@
+# Setzt den Pfad zum Ordner, der die .tex Dateien enthält
+input_directory <- 'myapp/tex/300_users'
+
+# Setzt den Pfad und Namen für die gesamte .tex Datei, die erstellt werden soll
+output_file <- 'myapp/tex/300_all.tex'
+
+# Listet alle .tex Dateien im Ordner auf
+tex_files <- list.files(path = input_directory, pattern = '\\.tex$', full.names = TRUE)
+
+# Öffnet die gesamte .tex Datei zum Schreiben
+fileConn <- file(output_file, 'w')
+
+# Geht durch jede .tex Datei
+for (tex_file in tex_files) {
+  # Liest den Inhalt der .tex Datei
+  content <- readLines(tex_file)
+  
+  # Schreibt den Inhalt in die gesamte .tex Datei
+  cat(content, file = fileConn, sep = '\n')
+  
+  # Fügt eine Leerzeile zwischen den Dateien ein
+  cat('\n', file = fileConn)
+}
+
+# Schließt die gesamte .tex Datei
+close(fileConn)
